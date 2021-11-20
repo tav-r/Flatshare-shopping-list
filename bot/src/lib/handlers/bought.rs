4c2a9bc7239fb5d6f559384ebfb8c110cece8b79
David Herrmann <david.herrmann@protonmail.com>
2021-11-20 17:42:04 +0100

    Handle empty shopping list better

--- bot/src/lib/handlers/bought.rs
+++ bot/src/lib/handlers/bought.rs
@@ -5,23 +5,32 @@ use teloxide::types::{KeyboardMarkup,KeyboardButton};
 pub async fn bought_handler(item: String, chat_id: i64) -> Result<Option<KeyboardMarkup>, &'static str> {
     if item.len() == 0 {
         get_shopping_list(chat_id)
+            .and_then(|l| if l.len() == 0 {
+                Err("Es steht nichts auf dem Einkaufszettel 🤷🏾")
+            } else {
+                Ok(l)
+            })
             .and_then(|l| Ok(Some(KeyboardMarkup::new::<Vec<Vec<KeyboardButton>>>(
                 l.iter().map(|s| 
                     vec!(KeyboardButton::new(format!("/eingekauft {}", s)))
                 ).collect()
             ))))
-            .or_else(|_| Err("Konnte die Liste nicht abrufen 😧"))
+            .or_else(|e| Err(e))
     } else {
-        set_shoppinglist(
-            chat_id,
-            get_shopping_list(chat_id)
-                .and_then(
-                    |l| Ok(l.into_iter().filter(
-                        |i| !&item.eq(i)
-                    ).collect::<Vec<String>>())
-                ).unwrap()
-        ).unwrap();
-
-        Ok(None)
+        get_shopping_list(chat_id)
+            .and_then(|l| if !l.iter().any(|i| i.eq(item.trim())) {
+                Err("Das steht nicht auf dem Einkaufszettel 🤔")
+            } else {
+                Ok(l)
+            })
+            .and_then(
+                |l| Ok(l.into_iter().filter(
+                    |i| !&item.eq(i)
+                ).collect::<Vec<String>>())
+            )
+            .and_then(
+                |v| { set_shoppinglist(chat_id, v).and_then(|_| Ok(None)).or_else(|e| Err(e)) }
+            )
+            .or_else(|e| Err(e))
     }
 }
